4bb2bd47ac087b41dda66b7d99e05ec8dfde714c
INDU1996 <indueee1996@gmail.com>
2017-07-06 12:14:52 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,20 @@
+package indu;
+
+public class PositiveNegative {
+	public static void main(String args[]){
+		int n=-6;
+		if (n<0)
+		{
+			System.out.println("it is negative number"+n);
+		}
+		if(n>0)
+		{
+			System.out.println("it is positive number"+n);}
+			if(n==0)
+			{
+				System.out.println("it is zero"+n);
+			}
+		}
+		
+	}
+
