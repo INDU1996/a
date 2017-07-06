264d78ef90ff338681ef1ff9337ae62c6c7ec2ec
INDU1996 <indueee1996@gmail.com>
2017-07-06 12:17:49 +0530

    Add files via upload

---
+++ EvnOdd.java
@@ -0,0 +1,15 @@
+package indu;
+
+public class EvnOdd {
+	public static void main(String args[]){
+		int n=12;
+		if(n%2==0)
+		{
+			System.out.println("the given number is even"+n);}
+			else
+			{
+				System.out.println("the given number is odd"+n);
+			}
+		}
+		}
+
