63aab75384ccc5a821f40709e91c8924ba862b56
INDU1996 <indueee1996@gmail.com>
2017-07-07 12:15:40 +0530

    Add files via upload

---
+++ SecondSmall.java
@@ -0,0 +1,23 @@
+package indu;
+
+public class SecondSmall {
+	  public static void main(String[] args) {
+
+		    int[] my_array = {-1, 4, 0, 2, 7, -3};
+		   
+		    int min = Integer.MAX_VALUE;
+		    int second_min = Integer.MAX_VALUE;
+		    for (int i = 0; i < my_array.length; i++) {
+		        if(my_array[i]==min){
+		          second_min=min;
+		        } else if (my_array[i] < min) {
+		            second_min = min;
+		            min = my_array[i];
+		        } else if (my_array[i] < second_min) {
+		            second_min = my_array[i];
+		        }
+
+		    }
+		    System.out.println("Second lowest number is : " + second_min);
+		    }
+		}
