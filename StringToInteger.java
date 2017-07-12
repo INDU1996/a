db87aff9bb37b6188554bb4a16812adf0cd80087
INDU1996 <indueee1996@gmail.com>
2017-07-12 12:43:08 +0530

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,15 @@
+package indu;
+
+public class StringToInteger {
+	 public static void main(String[] args) {
+		   
+		    
+		    
+		    Integer intObj1 = new Integer("100");
+		    System.out.println(intObj1);
+		    String str = "100";
+		    Integer intObj2 = Integer.valueOf(str);
+		    System.out.println(intObj2);
+		   
+}
+}
