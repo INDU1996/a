af38eb40144dcf55c02906c0b7d77b1abed877ca
INDU1996 <indueee1996@gmail.com>
2017-07-17 12:22:06 +0530

    Add files via upload

---
+++ MaxiLength.java
@@ -0,0 +1,19 @@
+package indu;
+
+import java.util.Scanner;
+
+public class MaxiLength {
+public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the String : ");
+		String str = scan.nextLine();
+		if(str.length()%2==0){
+			System.out.println("Length is : "+str.length());
+		}
+		else{
+			System.out.println("Length is : "+(str.length()-1));
+		}
+
+	}
+}
