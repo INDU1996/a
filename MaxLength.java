9a4f2f4950471cc1bc46ffba8c1954f2a625597c
INDU1996 <indueee1996@gmail.com>
2017-07-17 12:15:44 +0530

    Add files via upload

--- MaxLength.java
+++ MaxLength.java
@@ -3,33 +3,18 @@ package indu;
 import java.util.Scanner;
 
 public class MaxLength {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Enter the string");
-		Scanner s=new Scanner(System.in);
-String a=s.next();
-String b="";
-int i=0;
-int j=0;
-String max="";
-for(i=0;i<a.length();i++){
-	for(j=i+1;j<a.length();j++){
-		b=a.substring(i,j+1);
-		System.out.println(b);
-		if(max.length()<b.length()){
-			max=b;
-			
+public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the String : ");
+		String str = scan.nextLine();
+		if(str.length()%2==0){
+			System.out.println("Length is : "+str.length());
 		}
-     }
-	}
-System.out.println("Largest String is:"+max);
-String c=new StringBuffer(max).reverse().toString();	
-if(c.equals(a)){
-	System.out.println("");
-}
-else {
-	System.out.println(max);
-}
+		else{
+			System.out.println("Length is : "+(str.length()-1));
+		}
 
 	}
+
 }
